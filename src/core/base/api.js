d434a6eb4c293a397dee63d4dd41cf48f044897c
Jack Q <QiaoBo@outlook.com>
2017-09-30 15:16:27 +0800

    improve test coverage

--- src/core/base/api.js
+++ src/core/base/api.js
@@ -1,30 +1,37 @@
-export const TODO = (that, message = 'this function should be override in subclass') => {
+export const TODO = (that, message = '[TODO] this function should be override in subclass') => {
   throw message;
 };
 
 export class StreamCipher {
   key;
 
+  /**
+   * @returns {Generator<number>}
+   */
   get stream() {
-    TODO(this);
-    return undefined;
+    return TODO(this);
   }
 
   constructor(key) {
     this.key = key;
   }
 
+  /**
+   * @param {Uint8Array} data
+   * @returns {Uint8Array} encrypted message
+   */
   encrypt(data) {
-    TODO(this);
-    return data;
+    return TODO([this, data]);
   }
 
+  /**
+   * @param {Uint8Array} cipher
+   * @returns {Uint8Array} decrypted message
+   */
   decrypt(cipher) {
-    TODO(this);
-    return cipher;
+    return TODO([this, cipher]);
   }
 }
-
 
 const API = {};
 export default API;
---
+++ src/core/base/api.test.js
@@ -0,0 +1,22 @@
+import { StreamCipher, TODO } from './api';
+
+it('should throw default TODO message', () => expect(() => TODO(null)).toThrow(/TODO/));
+
+/**
+ * Check the required member of class contain TODO check
+ *
+ * @param {object} ClassName
+ * @param {String[]} requiredMembers
+ */
+const checkSubclassImplementation = (ClassName, requiredMembers) => {
+  class EmptyImpl extends ClassName { }
+  const inst = new EmptyImpl();
+  requiredMembers.map(m =>
+    it(`should check implementation of ${m} in subclass`, () =>
+      expect(() => (typeof inst[m] === 'function' ? inst[m]() : inst[m]))
+        .toThrow(/TODO/)),
+  );
+};
+
+
+checkSubclassImplementation(StreamCipher, ['stream', 'encrypt', 'decrypt']);
