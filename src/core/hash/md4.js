79b1a6fe2892279dee6fe03309c9f0a9c004703c
Jack Q <QiaoBo@outlook.com>
2017-10-03 16:18:07 +0800

    impl md4 hash

---
+++ src/core/hash/md4.js
@@ -0,0 +1,88 @@
+import { MD4HashBase } from './md4-family/base';
+
+const initState = Uint32Array.of(0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476);
+
+const indexTable = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+  0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15,
+  0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15,
+];
+
+const shiftAmount = [
+  3, 7, 11, 19, 3, 7, 11, 19, 3, 7, 11, 19, 3, 7, 11, 19,
+  3, 5, 9, 13, 3, 5, 9, 13, 3, 5, 9, 13, 3, 5, 9, 13,
+  3, 9, 11, 15, 3, 9, 11, 15, 3, 9, 11, 15, 3, 9, 11, 15,
+];
+
+/**
+ *
+ * @param {Uint32Array} state
+ * @param {Uint8Array} buffer
+ */
+const md4MainLoop = (state, buffer) => {
+  let [a, b, c, d] = state;
+
+  for (let i = 0; i < 48; i += 1) {
+    let f = 0;
+    let k = 0;
+
+    if (i < 16) {
+      f = (b & c) | (~b & d);
+      k = 0;
+    } else if (i < 32) {
+      f = (b & c) | (b & d) | (c & d);
+      k = 0x5a827999;
+    } else {
+      f = b ^ c ^ d;
+      k = 0x6ed9eba1;
+    }
+    const z = indexTable[i];
+    const mz =
+      (buffer[z * 4 + 3] << 24) |
+      (buffer[z * 4 + 2] << 16) |
+      (buffer[z * 4 + 1] << 8) |
+      (buffer[z * 4 + 0] << 0);
+
+    const t = a + f + mz + k;
+    a = d;
+    d = c;
+    c = b;
+
+    b = (t << shiftAmount[i]) | (t >>> (32 - shiftAmount[i]));
+  }
+
+  state[0] += a;
+  state[1] += b;
+  state[2] += c;
+  state[3] += d;
+};
+
+
+export class MD4Hash extends MD4HashBase {
+
+  constructor() {
+    super(MD4Hash, 'MD4');
+  }
+
+  mainLoop() {
+    md4MainLoop(this.state, this.buffer);
+  }
+
+  initState() {
+    this.state = new Uint32Array(4);
+  }
+
+  resetState() {
+    initState.forEach((v, i) => { this.state[i] = v; });
+  }
+
+  exportState() {
+    return new Uint8Array(Uint32Array.of(...this.state).buffer);
+  }
+
+  static hash(data) {
+    return new MD4Hash().hash(data);
+  }
+}
+
+export default MD4Hash;
---
+++ src/core/hash/md4.test.js
@@ -0,0 +1,20 @@
+import { MD4Hash } from './md4';
+import Encode from '../encode';
+
+const test = (msg, md) => it(`should hash "${msg}" to "${md}"`, () => expect(
+  Encode.Base16Encoder.encode(MD4Hash.hash(Uint8Array.from(msg.split('').map(i => i.charCodeAt(0))))),
+).toEqual(md.toUpperCase()));
+
+const testCases = [
+  ['', '31d6cfe0d16ae931b73c59d7e0c089c0'],
+  ['\0', '47c61a0fa8738ba77308a8a600f88e4b'],
+  ['Hello World', '77a781b995cf1cfaf39d9e2f5910c2cf'],
+  ['The quick brown fox jumps over the lazy dog', '1bee69a46ba811185c194762abaeae90'],
+  ['The quick brown fox jumps over the lazy dog.', '2812c6c7136898c51f6f6739ad08750e'],
+];
+
+testCases.forEach(i => test(...i));
+
+it('should hash large data correctly', () => expect(
+  Encode.Base16Encoder.encode(MD4Hash.hash(new Uint8Array(12000))),
+).toEqual('69011D94AE0D50D20321C0BE964C7257'));
