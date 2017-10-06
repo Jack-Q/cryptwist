954f078639b8a35bd07414b385781fcc6defeb92
Jack Q <QiaoBo@outlook.com>
2017-10-06 23:17:38 +0800

    keccak theta

--- src/core/hash/keccak/theta.js
+++ src/core/hash/keccak/theta.js
@@ -1,2 +1,36 @@
-export const theta = state => state;
+import { assign, retrieve, index, rotR } from './util';
+
+export const theta = (state) => {
+  const c = new Uint32Array(5 * 2);
+  const cs = new Uint32Array(5 * 2);
+  // update parity of echo column
+  for (let i = 0; i < 5; i += 1) {
+    const c0 = retrieve(state, index(i, 0));
+    const c1 = retrieve(state, index(i, 1));
+    const c2 = retrieve(state, index(i, 2));
+    const c3 = retrieve(state, index(i, 3));
+    const c4 = retrieve(state, index(i, 4));
+    const ci = [
+      c0[0] ^ c1[0] ^ c2[0] ^ c3[0] ^ c4[0],
+      c0[1] ^ c1[1] ^ c2[1] ^ c3[1] ^ c4[1],
+    ];
+    const csi = rotR(...ci, 1);
+    assign(c, i, ...ci);
+    assign(cs, i, ...csi);
+  }
+
+  // update the state
+  for (let x = 0; x < 5; x += 1) {
+    const d1 = retrieve(c, (x + 4) % 5);
+    const d2 = retrieve(cs, (x + 1) % 5);
+    const d = [d1[0] ^ d2[0], d1[1] ^ d2[1]];
+    for (let y = 0; y < 5; j += 1) {
+      const l = retrieve(state, index(x, y));
+      assign(state, index(x, y), ...[
+        l[0] ^ d[0],
+        l[1] ^ d[1],
+      ]);
+    }
+  }
+};
 export default theta;
---
+++ src/core/hash/keccak/util.js
@@ -0,0 +1,18 @@
+
+export const retrieve = (arr, ind) => [arr[ind * 2], arr[ind * 2 + 1]];
+
+export const assign = (arr, ind, h, l) => {
+  arr[ind * 2] = h;
+  arr[ind * 2 + 1] = l;
+  return [h, l];
+};
+
+export const index = (x, y) => x + 5 * y;
+
+export const rotR = (h, l, len) => (len > 32 ? [
+  (l >>> (len - 32)) | (h << (64 - len)),
+  (h >>> (len - 32)) | (l << (64 - len)),
+] : [
+  (h >>> len) | (l << (32 - len)),
+  (l >>> len) | (h << (32 - len)),
+]);
