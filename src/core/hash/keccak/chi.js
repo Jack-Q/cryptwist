8f9fd48341041aae58b2c68565d679de5ce8d09a
Jack Q <QiaoBo@outlook.com>
2017-10-06 23:50:26 +0800

    impl keccak chi

--- src/core/hash/keccak/chi.js
+++ src/core/hash/keccak/chi.js
@@ -1,2 +1,27 @@
-export const chi = state => state;
+import { assign, retrieve, index } from './util';
+
+export const chi = (state) => {
+  for (let y = 0; y < 5; y += 1) {
+    const t0 = retrieve(state, index(0, y));
+    const t1 = retrieve(state, index(1, y));
+    const t2 = retrieve(state, index(2, y));
+    const t3 = retrieve(state, index(3, y));
+    const t4 = retrieve(state, index(4, y));
+
+    const x0 = [t0[0] ^ (~t1[0] & t2[0]), t0[1] ^ (~t1[1] & t2[1])];
+    const x1 = [t1[0] ^ (~t2[0] & t3[0]), t1[1] ^ (~t2[1] & t3[1])];
+    const x2 = [t2[0] ^ (~t3[0] & t4[0]), t2[1] ^ (~t3[1] & t4[1])];
+    const x3 = [t3[0] ^ (~t4[0] & t0[0]), t3[1] ^ (~t4[1] & t0[1])];
+    const x4 = [t4[0] ^ (~t0[0] & t1[0]), t4[1] ^ (~t0[1] & t1[1])];
+
+    assign(state, index(0, y), ...x0);
+    assign(state, index(1, y), ...x1);
+    assign(state, index(2, y), ...x2);
+    assign(state, index(3, y), ...x3);
+    assign(state, index(4, y), ...x4);
+  }
+  return state;
+};
+
+
 export default chi;
--- src/core/hash/keccak/pi.js
+++ src/core/hash/keccak/pi.js
@@ -1,2 +1,16 @@
-export const pi = state => state;
+import { assign, retrieve, index } from './util';
+
+export const pi = (state) => {
+  const o = Uint32Array.from(state);
+  for (let x = 0; x < 5; x += 1) {
+    for (let y = 0; y < 5; y += 1) {
+      assign(state, index(x, y),
+        ...retrieve(o,
+          index((x + 3 * y) % 5, x),
+        ),
+      );
+    }
+  }
+  return state;
+};
 export default pi;
