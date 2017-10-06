7c5fca996c8f5e6f817e03079ce04d105dd4febb
Jack Q <QiaoBo@outlook.com>
2017-10-06 23:31:25 +0800

    impl kaccek rho

--- src/core/hash/keccak/rho.js
+++ src/core/hash/keccak/rho.js
@@ -1,2 +1,20 @@
-export const rho = state => state;
+import { assign, retrieve, index, rotR } from './util';
+
+export const rho = (state) => {
+  for (let t = 0, x = 1, y = 0; t < 24; t += 1) {
+    // shift the index
+    assign(state, index(x, y),
+      ...rotR(
+        ...retrieve(state, index(x, y)),
+        ((t + 1) * (t + 2) / 2) % 64,
+      ),
+    );
+
+    // update index
+    [x, y] = [y, (2 * x + 3 * y) % 5];
+  }
+  return state;
+};
+
+
 export default rho;
--- src/core/hash/keccak/theta.js
+++ src/core/hash/keccak/theta.js
@@ -32,5 +32,7 @@ export const theta = (state) => {
       ]);
     }
   }
+
+  return state;
 };
 export default theta;
