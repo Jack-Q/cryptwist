d8612d4f2330342dbe71919af327d03b99477137
Jack Q <QiaoBo@outlook.com>
2017-10-19 01:24:23 +0800

    abstract hmac interfcae

---
+++ src/core/hash/hmac.js
@@ -0,0 +1,64 @@
+import { Hash } from '../base/api';
+
+
+const iPadding = 0x36;
+const oPadding = 0x5c;
+
+export class Hmac extends Hash {
+
+  init(hashInst, blockLength, key) {
+    this.key = key;
+    this.blockLength = blockLength;
+    this.hashInst = hashInst;
+
+    if (this.key.length <= this.blockLength) {
+      this.padKey = new Uint8Array(this.blockLength).fill(0);
+      this.padKey.set(this.key);
+    } else {
+      this.padKey = hashInst.hash(this.key);
+    }
+    this.clean = true;
+  }
+
+  reset() {
+    this.clean = true;
+    this.hashInst.reset();
+  }
+
+  feedData(data) {
+    if (data && data.length > 0) {
+      if (this.clean) {
+        // first section
+        this.hashInst.feedData(this.padKey.map(v => v ^ iPadding));
+        this.clean = false;
+      }
+      this.hashInst.feedData(data);
+    }
+  }
+
+  endData(data) {
+    const result = this.hashInst.hash(Uint8Array.of(
+      ...this.padKey.map(v => v ^ oPadding),
+      ...this.hashInst.endData(data),
+    ));
+    this.reset();
+    return result;
+  }
+
+  hash(data) {
+    return this.hashInst.hash(Uint8Array.of(
+      ...this.padKey.map(v => v ^ oPadding),
+      ...this.hashInst.hash(Uint8Array.of(
+        ...this.padKey.map(v => v ^ iPadding),
+        ...data,
+      )),
+    ));
+  }
+
+  static hash(data, hashInst, blockLength, key) {
+    return new Hmac(hashInst, blockLength, key).hash(data);
+  }
+}
+
+
+export default { Hmac };
