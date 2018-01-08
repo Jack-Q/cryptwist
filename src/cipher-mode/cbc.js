e8896af1b294256d1c14ca308f838bc9eaea47e5
Jack Q <QiaoBo@outlook.com>
2018-01-09 07:57:10 +0800

    add cbc block cipher mode

---
+++ src/cipher-mode/cbc.js
@@ -0,0 +1,69 @@
+export class CBCCipherMode {
+  cipher;
+  dataBuffer;
+  blockSize = 0;
+  iv;
+
+  /**
+   * @param {} BlockCipher block cipher class
+   * @param {Uint8Array} key encryption key of cipher
+   * @param {Uint8Array} iv initialization vector
+   */
+  constructor(BlockCipher, key, iv /* cipherOption = [] */) {
+    if (!iv || iv.length !== BlockCipher.meta.blockSize) {
+      throw 'initialization vector should have the same size of the block size of cipher';
+    }
+    this.BlockCipher = BlockCipher;
+    this.blockSize = BlockCipher.meta.blockSize;
+    this.cipher = new BlockCipher(key);
+    this.iv = Uint8Array.from(iv);
+  }
+
+  reset() {}
+  feedData() { }
+  endData() { }
+
+  encrypt(buffer = this.dataBuffer) {
+    if (buffer.length % this.blockSize !== 0) {
+      throw 'input data is not block aligned';
+    }
+    const cipher = new Uint8Array(buffer);
+    const blockSize = this.blockSize;
+    const blockCount = buffer.length / blockSize;
+    let lastCipher = this.iv;
+    for (let i = 0; i < blockCount; i++) {
+      const offset = blockSize * i;
+      const plainBlock = buffer.slice(offset, offset + blockSize);
+      for (let j = 0; j < plainBlock.length; j++) {
+        plainBlock[j] ^= lastCipher[j];
+      }
+      const cipherBlock = this.cipher.encrypt(plainBlock);
+      cipher.set(cipherBlock, offset);
+      lastCipher = cipherBlock;
+    }
+    return cipher;
+  }
+
+  decrypt(buffer = this.dataBuffer) {
+    if (buffer.length % this.blockSize !== 0) {
+      throw 'input data is not block aligned';
+    }
+    const cipher = new Uint8Array(buffer);
+    const blockSize = this.blockSize;
+    const blockCount = buffer.length / blockSize;
+    let lastCipher = this.iv;
+    for (let i = 0; i < blockCount; i++) {
+      const offset = blockSize * i;
+      const cipherBlock = buffer.slice(offset, offset + blockSize);
+      const plainBlock = this.cipher.decrypt(cipherBlock);
+      for (let j = 0; j < plainBlock.length; j++) {
+        plainBlock[j] ^= lastCipher[j];
+      }
+      cipher.set(plainBlock, offset);
+      lastCipher = cipherBlock;
+    }
+    return cipher;
+  }
+}
+
+export default CBCCipherMode;
